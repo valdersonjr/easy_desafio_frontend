069527915f9f1f7774979a33b66b1f61acc98905
valdersonjr <valdersonpontes@gmail.com>
2024-01-23 13:44:09 -0300

    :lipstick: [UPDATE] Color theme changed

--- src/services/vuestic-ui/themes.ts
+++ src/services/vuestic-ui/themes.ts
@@ -3,54 +3,18 @@ import { useGlobalConfig } from 'vuestic-ui'
 
 export const THEME_NAMES = {
   LIGHT: 'LIGHT',
-  ORIGINAL: 'ORIGINAL',
-  DARK: 'DARK',
-  SEMI_DARK: 'SEMI_DARK',
 }
 
 export const COLOR_THEMES = [
   {
     name: THEME_NAMES.LIGHT,
     colors: {
-      primary: '#154ec1',
-      secondary: '#767c88',
+      primary: '#FF0000',
+      secondary: '#1E293B',
       success: '#3d9209',
       info: '#2c82e0',
       danger: '#e42222',
       warning: '#ffd43a',
-    },
-  },
-  {
-    name: THEME_NAMES.DARK,
-    colors: {
-      primary: '#6F92DA',
-      secondary: '#A8AEBA',
-      success: '#3d9209',
-      info: '#2c82e0',
-      danger: '#e42222',
-      warning: '#ffd43a',
-    },
-  },
-  {
-    name: THEME_NAMES.SEMI_DARK,
-    colors: {
-      primary: '#6F92DA',
-      secondary: '#A8AEBA',
-      success: '#3d9209',
-      info: '#2c82e0',
-      danger: '#e42222',
-      warning: '#ffd43a',
-    },
-  },
-  {
-    name: THEME_NAMES.ORIGINAL,
-    colors: {
-      primary: '#6EDC7D',
-      secondary: '#A8AEBA',
-      success: '#76E18D',
-      info: '#2c82e0',
-      danger: '#e34b4a',
-      warning: '#ffc200',
     },
   },
 ]
