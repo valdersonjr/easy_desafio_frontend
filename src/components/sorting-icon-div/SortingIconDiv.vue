bdd6873c60d2d003b4c4cfc8e8baebae1881b36f
valdersonjr <valdersonpontes@gmail.com>
2024-03-14 12:22:15 -0300

    :sparkles: [UPDATE] Creating sorting component

---
+++ src/components/sorting-icon-div/SortingIconDiv.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-row gap-1 items-center">
+    <span class="select-none">{{ props.text }}</span>
+    <va-icon size="3" name="vuestic-iconset-sort-arrow" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+  const props = defineProps(['text'])
+</script>
