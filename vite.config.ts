2bf71f402d803c013ccc0ea631cd82cb80b41149
valdersonjr <valdersonpontes@gmail.com>
2024-06-11 13:05:20 -0300

    [ADD] Port to .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'url'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +16,7 @@ export default defineConfig({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
     }),
   ],
-})
+  server: {
+    port: parseInt(process.env.PORT ?? '3000'),
+  },
+});
